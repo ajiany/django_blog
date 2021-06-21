cd0aba14fb09b8a1d849e954058886ef9e95b1f1
ajian <1641574839@qq.com>
2021-06-22 00:46:04 +0800

    database configuring

--- CooperBlog/CooperBlog/settings.py
+++ CooperBlog/CooperBlog/settings.py
@@ -73,11 +73,22 @@ WSGI_APPLICATION = 'CooperBlog.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+        'ENGINE': 'django.db.backends.mysql', # 数据库引擎
+        'HOST': '127.0.0.1', # 数据库主机
+        'PORT': 3306, # 数据库端口
+        'USER': 'Cooper', # 数据库用户名
+        'PASSWORD': '8xujiajian!', # 数据库用户密码
+        'NAME': 'django_mysql' # 数据库名字
+    },
 }
 
 
